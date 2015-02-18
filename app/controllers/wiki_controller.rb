aa914d75621421ff861bccd72924a6deb0759c4d
Esther Zinn <Bionicfemme@me.com>
2015-02-18 10:22:05 -0500

    Added user account authorization

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -1,6 +1,7 @@
 class WikiController < ApplicationController
   def index
     @wikis = Wiki.all
+      authorize @wiki
   end
 
   def show
@@ -9,10 +10,12 @@ class WikiController < ApplicationController
 
   def new
     @wiki = Wiki.new
+      authorize @wiki
   end
 
   def create
     @wiki = Wiki.new(params.require(:wiki).permit(:title, :body))
+      authorize @wiki
     if @wiki.save
       flash[:notice] = "Wiki was saved."
       redirect_to @wiki
@@ -24,10 +27,12 @@ class WikiController < ApplicationController
 
   def edit
     @wiki = Wiki.find(params[:id])
+      authorize @wiki
   end
 
   def update
      @wiki = Wiki.find(params[:id])
+      authorize @wiki
      if @wiki.update_attributes(params.require(:wiki).permit(:title, :body))
        flash[:notice] = "Wiki was updated."
        redirect_to @wiki
